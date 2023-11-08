12a4b3357fd9120be50bb9fbc322c2b6091eed57
luizfelipelaviola <luiz@laviola.dev>
2023-11-07 23:41:26 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+// https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+  
+function computeLPSArray(pat, M, lps)
+{
+    // length of the previous longest prefix suffix
+    var len = 0;
+    var i = 1;
+    lps[0] = 0; // lps[0] is always 0
+  
+    // the loop calculates lps[i] for i = 1 to M-1
+    while (i < M) {
+        if (pat.charAt(i) == pat.charAt(len)) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (pat[i] != pat[len])
+        {
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar
+            // to search step.
+            if (len != 0) {
+                len = lps[len - 1];
+  
+                // Also, note that we do not increment
+                // i here
+            }
+            else // if (len == 0)
+            {
+                lps[i] = len;
+                i++;
+            }
+        }
+    }
+
+    console.log("LPS: " + pat);
+    console.log("     " + lps.join(''));
+    console.log(" ");
+}
+  
+function KMPSearch(pat,txt)
+{
+    var M = pat.length;
+    var N = txt.length;
+  
+    // create lps[] that will hold the longest
+    // prefix suffix values for pattern
+    var lps = [];
+    var j = 0; // index for pat[]
+    var results = [];
+  
+    // Preprocess the pattern (calculate lps[] array)
+    computeLPSArray(pat, M, lps);
+  
+    var i = 0; // index for txt[]
+    while ((N - i) >= (M - j)) {
+        if (pat.charAt(j) == txt.charAt(i)) {
+            j++;
+            i++;
+        }
+        if (j == M) {
+            console.log("Found pattern " + "at index " + (i - j) + "\n");
+            results.push(i-j);
+            j = lps[j - 1];
+        }
+  
+        // mismatch after j matches
+        else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+
+    if(results.length) {
+        console.log("Completed. Found pattern at indexes: " + results.join(', '));
+        console.log("Total matches: " + results.length);
+        console.log(txt)
+        for(var i = 0; i < txt.length; i++) {
+            if(results.includes(i)) {
+              for(var j = 0; j < pat.length; j++) {
+                process.stdout.write('^');
+              }
+              i += pat.length - 1;
+            } else {
+                process.stdout.write(' ');
+            }
+        }
+    }
+}
+
+var txt = "ABABDABACDABABCABAB";
+var pat = "ABABCABAB";
+
+KMPSearch(pat, txt);
